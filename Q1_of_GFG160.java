dfd89d921dbb5168833331d06beeabd26cb7025b
Vivek Chavda <115214436+Vivekchavda1374@users.noreply.github.com>
2024-12-01 19:46:24 +0530

    Create Q1_of_GFG160.java

---
+++ Q1_of_GFG160.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        
+        int firstMax = -1;
+        int secondMax = -1;
+        
+        for (int num : arr) {
+            if (num > firstMax) {
+                secondMax = firstMax;
+                firstMax = num;
+            } else if (num > secondMax && num != firstMax) {
+               
+                secondMax = num;
+            }
+        }
+        return secondMax != -1 ? secondMax : -1;
+    }
+}
